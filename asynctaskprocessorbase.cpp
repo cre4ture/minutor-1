14e686b9fe66e9a3d99a950c6005e8e79b7719ff
Ulrich Hornung <hornunguli@gmx.de>
2019-02-23 20:59:07 +0100

    addition to last

--- asynctaskprocessorbase.cpp
+++ asynctaskprocessorbase.cpp
@@ -1,6 +1,40 @@
 #include "asynctaskprocessorbase.hpp"
 
-AsyncTaskProcessorBase::AsyncTaskProcessorBase()
-{
+#include <future>
 
-}
+class AsyncTaskProcessorBase::ImplC
+{
+public:
+    ImplC(AsyncTaskProcessorBase& parent)
+        : m_parent(parent)
+    {
+        for (size_t i = 0; i < 8; i++)
+        {
+             m_futures.push_back(std::async(std::launch::async, [this]() {
+                AsyncTaskProcessorBase::JobT job;
+                while (m_queue.pop(job))
+                {
+                    job();
+                }
+            }));
+        }
+    }
+
+    ~ImplC()
+    {
+        m_queue.signalTerminate();
+        for (auto& future: m_futures)
+        {
+            future.get();
+        }
+    }
+
+    AsyncTaskProcessorBase& m_parent;
+    ThreadSafeQueue<AsyncTaskProcessorBase::JobT> m_queue;
+    std::list<std::future<void> > m_futures;
+};
+
+AsyncTaskProcessorBase::AsyncTaskProcessorBase()
+    : m_impl(QSharedPointer<ImplC>::create(*this))
+    , m_queue(m_impl->m_queue)
+{}
--- asynctaskprocessorbase.hpp
+++ asynctaskprocessorbase.hpp
@@ -1,11 +1,28 @@
 #ifndef ASYNCTASKPROCESSORBASE_HPP
 #define ASYNCTASKPROCESSORBASE_HPP
 
+#include "threadsafequeue.hpp"
+
+#include <QSharedPointer>
 
 class AsyncTaskProcessorBase
 {
 public:
+    typedef std::function<void()> JobT;
+
     AsyncTaskProcessorBase();
+
+    void enqueueJob(const JobT& job)
+    {
+        m_queue.push(job);
+    }
+
+protected:
+    class ImplC;
+
+    QSharedPointer<ImplC> m_impl;
+    ThreadSafeQueue<JobT>& m_queue;
 };
+
 
 #endif // ASYNCTASKPROCESSORBASE_HPP
