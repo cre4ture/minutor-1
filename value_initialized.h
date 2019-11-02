9c972713fbb45fdf734e0618c4fe58f69b10979d
Ulrich Hornung <hornunguli@gmx.de>
2019-11-02 23:05:07 +0100

    addition to last

--- value_initialized.h
+++ value_initialized.h
@@ -1,4 +1,16 @@
 #ifndef VALUE_INITIALIZED_H
 #define VALUE_INITIALIZED_H
 
+template<class T>
+class value_initialized
+{
+  public :
+    value_initialized() : x() {}
+    operator T const &() const { return x ; }
+    operator T&() { return x ; }
+    value_initialized<T> operator=(const T& o) { x = o; return *this; }
+  private :
+    T x ;
+} ;
+
 #endif // VALUE_INITIALIZED_H
